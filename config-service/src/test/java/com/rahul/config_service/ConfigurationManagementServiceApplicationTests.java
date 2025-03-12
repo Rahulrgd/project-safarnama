09cf7ebc41e6d2adea21c0b63489c86a2354fc4f
RahulGupta <rahulrgd21@gmail.com>
2025-03-12 14:01:00 +0530

    Added initial commit

---
+++ config-service/src/test/java/com/rahul/config_service/ConfigurationManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
